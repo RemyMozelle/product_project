b0b2ae2538d28eafa5e0894752e18ba870399d0a
RemyMozelle <timoz974@hotmail.fr>
2018-04-11 20:47:35 +0400

    feat(users): add User Model, add route GET: /users add route POST: /users

--- src/app.js
+++ src/app.js
@@ -5,16 +5,19 @@ import createSequelize from "./database/db";
 import modelCategory from "./models/Categories";
 import modelProduct from "./models/Products";
 import modelComment from "./models/Comment";
+import modelUser from "./models/Users";
 // ROUTES
 import categories from "./routes/categories";
 import products from "./routes/products";
-import comments from './routes/comments';
+import comments from "./routes/comments";
+import users from "./routes/users";
 // SEQUELIZE
 const sequelize = createSequelize();
 // IMPORT MODELS
 const Category = sequelize.import("categories", modelCategory);
 const Products = sequelize.import("products", modelProduct);
-const Comments = sequelize.import("comments", modelComment); 
+const Comments = sequelize.import("comments", modelComment);
+const Users = sequelize.import("users", modelUser);
 // RELATIONSHIP
 Category.belongsTo(Category);
 Comments.hasMany(Products);
@@ -26,5 +29,6 @@ app.use(bodyParser.json());
 products(app, Products);
 categories(app, Category);
 comments(app, Comments);
+users(app, Users);
 
 app.listen(3001);

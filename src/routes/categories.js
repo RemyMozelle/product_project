a7c90a76fdd719e172b1792496422b58d921ca1d
Alexandre Callens <alekzxand@gmail.com>
2018-04-11 10:41:33 +0400

    check

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,24 +1,24 @@
 module.exports = (app, Category) => {
   // AFFICHER TOUTES LES CATEGORIES
-  app.get("/display_all", (req, res) => {
+  app.get("/categories_display_all", (req, res) => {
     Category.findAll().then(category => {
       res.json({ category });
     });
   });
   // AFFICHER LES CATEGORIES ACTIVER
-  app.get("/display", (req, res) => {
-    Category.findAll({ where: { isActive: 0 } }).then(category => {
+  app.get("/categories_display", (req, res) => {
+    Category.findAll({ where: { isActive: 1 } }).then(category => {
       res.json({ category });
     });
   });
   // AFFICHER LES CATEGORIES DESACTIVER
-  app.get("/disabled", (req, res) => {
+  app.get("/categories_disabled", (req, res) => {
     Category.findAll({ where: { isActive: 0 } }).then(category => {
       res.json({ category });
     });
   });
   // CREER UNE CATEGORIE
-  app.get("/create", (req, res) => {
+  app.get("/categories_create", (req, res) => {
     const category = {
       name: req.body.name,
       categoryId: req.body.category,

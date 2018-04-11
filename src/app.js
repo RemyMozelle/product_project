b5e1138dacd37daccf9de009f724298899ea4c73
Alexandre Callens <alekzxand@gmail.com>
2018-04-11 10:28:02 +0400

    router sequelize

--- src/app.js
+++ src/app.js
@@ -2,9 +2,20 @@ import express from "express";
 import bodyParser from "body-parser";
 import categories from "./routes/categories";
 import products from "./routes/products";
+import createSequelize from "./database/db";
+import modelCategory from "./models/Categories";
+import modelProduct from "./models/Products";
+
+const sequelize = createSequelize();
+const Category = sequelize.import("categories", modelCategory);
+const Products = sequelize.import("products", modelProduct);
+
+// RELATIONSHIP
+Category.belongsTo(Category);
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/categories", categories);
-app.use("/products", products);
+
+products(app, Products);
+categories(app, Category);
 app.listen(3001);
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,9 +1,1 @@
-import { Router } from "express";
-import createSequelize from "./../database/db";
-import modelProduct from "./../models/Products";
-
-const sequelize = createSequelize();
-const route = Router();
-const Products = sequelize.import("products", modelProduct);
-
-module.exports = route;
+module.exports = (app, Products) => {};

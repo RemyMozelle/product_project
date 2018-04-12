d8c73a3832b48f77fae3d284754c409fb80145b7
hiromeister <hermandrobin@gmail.com>
2018-04-12 14:11:06 +0400

    Add SQL scripts for each requests except POST

---
+++ requests.sql
@@ -0,0 +1,14 @@
+-- PRODUCTS
+  -- Display all products
+  SELECT `id`, `name`, `price`, `categories_id` FROM `products` AS `products`
+  -- Create a product
+
+  -- Display all products by ascending
+  SELECT `id`, `name`, `price`, `categories_id` FROM `products` AS `products` ORDER BY `price`;
+  -- Display all products by descending
+  SELECT `id`, `name`, `price`, `categories_id` FROM `products` AS `products` ORDER BY `products`.`price` DESC;
+  -- Display all products from a particular category 
+   SELECT `id`, `name`, `price`, `categories_id` FROM `products` AS `products` WHERE `products`.`categories_id` = 1;
+   -- Display sorted by ascending price product from a particular category
+   SELECT `id`, `name`, `price`, `categories_id` FROM `products` AS `products` WHERE `products`.`categories_id` = 1 ORDER BY `price`;
+
